19c4ed46053a85c1905c8acaa451e92010bc4b1b
bsdaoquang <bsdaoquang@gmail.com>
2023-08-18 10:01:03 +0700

    fix by mentor

--- posts/post-detail.js
+++ posts/post-detail.js
@@ -1,18 +1,44 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const postId = urlParams.get('id')
 
-const postId = urlParams.get('postId')
+let posts  = [] 
 
-document.getElementById('post-container').innerHTML = `
-<div class="post-box food">
-            <img src="https://image.cnbcfm.com/api/v1/image/107283868-1691590732310-107283868-1691590529979-gettyimages-1235707269-PGONCHAR_W0863.jpg?v=1691590756&w=717&h=403&ffmt=webp&vtcrop=y" alt="" class="post-img">
-            <h2 class="category">Tech</h2>
-            <a href="./posts/post-detail.html?postId=1" class="post-title">Roblox shares drop 21% after company misses estimates on top and bottom line</a>
-            <span class="post-date">12 Feb 2022</span>
-            <p class="post-description">The company also said that Cameron Deatsch, who has worked as chief revenue officer for the past three and a half years, will leave in December.</p>
-            <div class="profile">
-                <img src="https://social.webestica.com/assets/images/avatar/06.jpg" alt="" class="profile-img">
-                <span class="profile-name">dany</span>
-            </div>
-        </div>
-`
+const getPosts = async () => {
+    const res =  localStorage.getItem('posts')
+    posts = res ? JSON.parse(res) : []
+}
+
+getPosts()
+
+
+const handlePostDetail = async () => {
+
+    if (posts.length > 0) {
+        const post = posts[postId]
+        
+        const img = document.createElement('img')
+        const h1 = document.createElement('h1')
+        const h3 = document.createElement('h3')
+        const content = document.createElement('div')
+
+        const container = document.createElement('div')
+
+        img.setAttribute('src', post.imgUrl)
+        h1.innerHTML = post.title 
+        h3.innerHTML = post.description
+        content.innerHTML = post.content
+
+        container.appendChild(img)
+        container.appendChild(h1)
+        container.appendChild(h3)
+        container.appendChild(content)
+
+        document.getElementById('post-detail').appendChild(container)
+
+    }else{
+        document.getElementById('post-detail').innerHTML = `Posts not found!`
+    }
+}
+
+handlePostDetail()
